40b81b7433abe1dc750c4b79000b4e423d3308f5
Michael Douglas <michaelneves92@gmail.com>
2020-09-06 03:48:13 -0300

    add config webhook for netlify

--- api/land-page/models/land-page.js
+++ api/land-page/models/land-page.js
@@ -1,8 +1,20 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+const { afterCreateHook, afterUpdateHook } = require("../../../hooks/netlify");
+
+module.exports = {
+  lifecycles: {
+    async afterCreate(result, data) {
+      afterCreateHook();
+    },
+
+    async afterUpdate(result, params, data) {
+      afterUpdateHook();
+    },
+  },
+};
