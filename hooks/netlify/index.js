817f875aa4849bce651aa33db8328a7751306de0
Michael Douglas <michaelneves92@gmail.com>
2020-09-06 03:58:03 -0300

    fix export config webhook for netlify

--- hooks/netlify/index.js
+++ hooks/netlify/index.js
@@ -1,11 +1,12 @@
 const axios = require("axios");
-
 const netlifyWebhook = strapi.config.get("custom.netlifyWebhook", null);
 
-export const afterCreateHook = () => {
-  netlifyWebhook && axios.post(netlifyWebhook);
-};
+module.exports = {
+  afterCreateHook: () => {
+    netlifyWebhook && axios.post(netlifyWebhook);
+  },
 
-export const afterUpdateHook = () => {
-  netlifyWebhook && axios.post(netlifyWebhook);
+  afterUpdateHook: () => {
+    netlifyWebhook && axios.post(netlifyWebhook);
+  },
 };
